42f6fa3038e2a54d7e709f7bce83ca4f281198e9
Satyam Sagar <satyamsagar1711@gmail.com>
2022-10-02 15:52:27 +0530

    added my details and changed previous commit back to original

--- src/assets/ContributorData.js
+++ src/assets/ContributorData.js
@@ -8,6 +8,14 @@ export const contributorData = [
         contributorTwitter:'https://twitter.com/smk_codes',
         contributorImage: 'https://user-images.githubusercontent.com/36791174/193437869-5e36f67d-d29e-4f6c-8781-bd353e20111e.JPG'
     },
+    {
+        contributorName: 'Satyam Sagar',
+        contributorLinkedIn:'https://www.linkedin.com/in/satyamrs00/',
+        contributorGithub: 'https://github.com/satyamrs00',
+        contributorIG: 'https://instagram.com/satyamrs00',
+        contributorTwitter:'https://twitter.com/satyamrs00',
+        contributorImage: 'https://avatars.githubusercontent.com/u/96380323'
+    },
     
 
    
