7a8f6a41da262935a98ab149e14f0b4b7a10df83
Tushar Kapoor <82567111+Kapoor-Tushar@users.noreply.github.com>
2022-10-04 13:10:18 +0530

    Created a Home Button using JavaScript
    In this code, I have considered index.html file present in public folder to be the initial Structure for the HTML file.
    Code is divided into following factors:-
    1) Removing noscript tag to enable script as index.html file present in public folder contains this tag.
    2)Creating a button and then adding the class and name to it.
    3) Adding a link to the button when it is clicked.
    Note - Here the link that I have added is the Dev Nation contributor page that you have created.

---
+++ src/components/Contributors/Contributors-HomeButton.js
@@ -0,0 +1,27 @@
+"use-strict"
+// Initailly script has been disablled in the public index.html document so first we need to enable it as it is the initial structure.
+
+let creatButton = function() {
+	// Selecting noscript tag from html file
+	let noScript = document.getElementsByTagName('noscript');
+	// Taking the target element from the HTML collection.
+	let tgtNoScript = noScript[0];
+	// Removing the noscript tag
+	tgtNoScript.parentNode.removeChild(tgtNoScript);
+
+	// Next step will be to create a home button
+
+	// Selecting parent class 
+	let btnParentClass = document.querySelector('#root');
+	// Creating the home button.
+	let homeButton = document.createElement('button');
+	homeButton.className = "btn";
+	homeButton.innerHTML = "Home";
+	// Adding home button to HTML
+	btnParentClass.appendChild(homeButton);
+	// Adding link to home button.
+	homeButton.addEventListener('click',function() {
+		window.location.href = "https://soundaryakhanapur.github.io/devnation-hacktoberfest/#/contributor";
+	})
+}
+creatButton();
