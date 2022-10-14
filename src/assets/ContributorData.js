c93de3bb6d86922a45356e00a4055e076d723457
Sharatheralagi007 <sharatheralagi@gmail.comgit config --global user.email sharatheralagi@gmail.com>
2022-10-14 17:28:46 +0530

    first commit

--- src/assets/ContributorData.js
+++ src/assets/ContributorData.js
@@ -28,6 +28,13 @@ export const contributorData = [
         contributorImage: 'https://avatars.githubusercontent.com/u/96380323'
 },
     
-
+{
+  contributorName: 'Sharath Heralagi',
+  contributorLinkedIn:'www.linkedin.com/in/sharat-heralagi-83a9b0220',
+  contributorGithub: 'https://github.com/Sharatheralagi007',
+  contributorIG: 'https://www.instagram.com/sharatheralagi/',
+  contributorTwitter:'',
+  contributorImage: 'https://user-images.githubusercontent.com/106471249/195834284-b52d8afc-446a-4cc1-9c23-3fd7c99199c2.jpg'
+},
    
 ];
