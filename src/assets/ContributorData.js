771b6b0f60fa626d0f73ab8ef1e6fceddd5845f9
Prateesh-Sulikeri <sulikeriprateesh7@gmail.com>
2022-10-14 16:56:31 +0530

    girst commit

--- src/assets/ContributorData.js
+++ src/assets/ContributorData.js
@@ -1,33 +1,42 @@
 
 export const contributorData = [
-    {
-        contributorName: 'Soundarya',
-        contributorLinkedIn:'https://www.linkedin.com/in/soundarya-khanapur/',
-        contributorGithub: 'https://github.com/SoundaryaKhanapur',
-        contributorIG: 'https://instagram.com/smk.codes',
-        contributorTwitter:'https://twitter.com/smk_codes',
-        contributorImage: 'https://user-images.githubusercontent.com/36791174/193437869-5e36f67d-d29e-4f6c-8781-bd353e20111e.JPG'
-    },
+  {
+    contributorName: 'Soundarya',
+    contributorLinkedIn: 'https://www.linkedin.com/in/soundarya-khanapur/',
+    contributorGithub: 'https://github.com/SoundaryaKhanapur',
+    contributorIG: 'https://instagram.com/smk.codes',
+    contributorTwitter: 'https://twitter.com/smk_codes',
+    contributorImage: 'https://user-images.githubusercontent.com/36791174/193437869-5e36f67d-d29e-4f6c-8781-bd353e20111e.JPG'
+  },
 
-      {  contributorName: 'FreemRL',
-        contributorLinkedIn:'https://www.linkedin.com/in/jannik-schmidtke-329926247/',
-        contributorGithub: 'https://github.com/FreemRL',
-        contributorIG: 'https://instagram.com/jannik27s',
-        contributorTwitter:'https://twitter.com/freem_RL',
-        contributorImage: 'https://avatars.githubusercontent.com/u/66525499?v=4'
+  {
+    contributorName: 'FreemRL',
+    contributorLinkedIn: 'https://www.linkedin.com/in/jannik-schmidtke-329926247/',
+    contributorGithub: 'https://github.com/FreemRL',
+    contributorIG: 'https://instagram.com/jannik27s',
+    contributorTwitter: 'https://twitter.com/freem_RL',
+    contributorImage: 'https://avatars.githubusercontent.com/u/66525499?v=4'
 
-    },
-    
-       {
+  },
 
-        contributorName: 'Satyam Sagar',
-        contributorLinkedIn:'https://www.linkedin.com/in/satyamrs00/',
-        contributorGithub: 'https://github.com/satyamrs00',
-        contributorIG: 'https://instagram.com/satyamrs00',
-        contributorTwitter:'https://twitter.com/satyamrs00',
-        contributorImage: 'https://avatars.githubusercontent.com/u/96380323'
-},
-    
+  {
 
-   
+    contributorName: 'Satyam Sagar',
+    contributorLinkedIn: 'https://www.linkedin.com/in/satyamrs00/',
+    contributorGithub: 'https://github.com/satyamrs00',
+    contributorIG: 'https://instagram.com/satyamrs00',
+    contributorTwitter: 'https://twitter.com/satyamrs00',
+    contributorImage: 'https://avatars.githubusercontent.com/u/96380323'
+  },
+
+  {
+
+    contributorName: 'Prateesh Sulikeri',
+    contributorLinkedIn: 'https://www.linkedin.com/in/prateesh-sulikeri-380986227',
+    contributorGithub: 'https://github.com/Prateesh-Sulikeri',
+    contributorIG: 'https://www.instagram.com/prateesh07/?next=%2F',
+    contributorTwitter: '',
+    contributorImage: 'https://user-images.githubusercontent.com/106986640/195834523-f8be89f3-a659-4c38-96f9-fa6d440004b2.jpg'
+  },
+
 ];
